e9278082da2bacbe1f5534ca171af2461fce4c38
rdawson24 <ryan.dawson@student.uts.edu.au>
2021-11-02 21:37:21 +1100

    Completed cherry functionality

--- Assets/Scripts/Level 1/AudioController.cs
+++ Assets/Scripts/Level 1/AudioController.cs
@@ -9,11 +9,16 @@ public class AudioController : MonoBehaviour
     public AudioSource pacSound;
     public AudioClip introClip;
     public AudioClip gameplayClip;
+    public AudioClip pacEat;
+    public AudioClip pacWalk;
+    private PacStudentController pacController;
 
     // Start is called before the first frame update
     void Start()
     {
         background.PlayOneShot(introClip, 1.0f);
+        pacController = GameObject.FindWithTag("Player").GetComponent<PacStudentController>();
+        pacSound.loop = true;
     }
 
     // Update is called once per frame
@@ -23,5 +28,21 @@ public class AudioController : MonoBehaviour
             background.clip = gameplayClip;
             background.Play();
         }
+        if(pacController.levelMap[pacController.playerPosy, pacController.playerPosx] == 5 && pacSound.clip != pacEat){
+            playEating();
+        }
+        if(pacController.levelMap[pacController.playerPosy, pacController.playerPosx] == 0 && pacSound.clip != pacWalk){
+            playWalking();
+        }
+    }
+
+    void playEating(){
+        pacSound.clip = pacEat;
+        pacSound.Play();
+    }
+
+    void playWalking(){
+        pacSound.clip = pacWalk;
+        pacSound.Play();
     }
 }
